7e22b6051f783d78e2af11b6399b9a2483f97fbd
Andrew Kubera <andrewkubera@gmail.com>
2015-06-24 04:19:31 -0400

    Formatting and docs

--- src/drivers/odbc/odbc_driver.hpp
+++ src/drivers/odbc/odbc_driver.hpp
@@ -23,27 +23,64 @@
 #include <sqltypes.h>
 
 /**
- * CppDB Driver for ODBC connections
+ * \class odbc_driver
+ * \brief CppDB Driver for ODBC connections
  */
 class odbc_driver : public cppdb::Driver {
 public:
+  /**
+   * Default Constructor
+   */
   odbc_driver();
+
+  /**
+   * Copy Constructor
+   */
   odbc_driver(const odbc_driver& orig);
+
+  /**
+   * Destructor
+   */
   virtual ~odbc_driver();
 
-  cppdb::status_t Connect(const std::string& host, const std::string& database, cppdb::port_t port, const std::string& username, const std::string& password);
+  /**
+   * Create a connection to a database from a set of parameters
+   */
+  cppdb::status_t Connect(const std::string& host,
+                          const std::string& database,
+                          cppdb::port_t port,
+                          const std::string& username,
+                          const std::string& password);
+
+
+  /**
+   * Disconnect from the database
+   */
   cppdb::status_t Disconnect();
+
   void Query();
 
+  /**
+   * Execute a query
+   *
+   * @param query: Database query to be executed
+   */
   template <typename... Tv>
-  cppdb::QueryResult<Tv...> ExecQuery(const cppdb::Query<Tv...>&);
+  cppdb::QueryResult<Tv...> ExecQuery(const cppdb::Query<Tv...>& query);
 
   template <typename... Tv>
   cppdb::QueryResult<Tv...> Exec(cppdb::Connection&);
 
 protected:
+  /**
+   * Internal method wrapping the ODBC method 'SQLBindCol'
+   */
   template <typename T>
-  void BindColumn(SQLHSTMT stmt, SQLUSMALLINT col_num, SQLPOINTER  target_val_ptr, SQLLEN buffsize, SQLLEN * strlen);
+  void BindColumn(SQLHSTMT stmt,
+                  SQLUSMALLINT col_num,
+                  SQLPOINTER target_val_ptr,
+                  SQLLEN buffsize,
+                  SQLLEN * strlen);
 
 protected:
   // sql environment
@@ -53,7 +90,7 @@ protected:
   SQLHDBC _hdbc;
 
   // sql status
-  char  _sql_stat[10];
+  char _sql_stat[10];
 };
 
 // callable functions from outside the driver
