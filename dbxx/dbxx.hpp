b1a7323638f8c28ecc846a00e4f54580f62d9a74
Andrew Kubera <andrewkubera@gmail.com>
2016-09-19 19:32:51 -0400

    Added trivial header files

---
+++ dbxx/dbxx.hpp
@@ -0,0 +1,16 @@
+/**
+ * \file dbxx/dbxx.hpp
+ *
+ * Main header for the dbxx database connection library.
+ *
+ */
+
+#pragma once
+
+#ifndef DBXX_HPP
+#define DBXX_HPP
+
+#include "dbxx_macros.hpp"
+
+
+
---
+++ dbxx/dbxx_macros.hpp
@@ -0,0 +1,13 @@
+/**
+ * \file dbxx/dbxx_macros.hpp
+ *
+ * Defines project-wide macros to be used internally.
+ */
+
+
+#ifndef DBXX_NAMESPACE_BEGIN
+
+#define DBXX_NAMESPACE_BEGIN namespace dbxx {
+#define DBXX_NAMESPACE_END }
+
+#endif
